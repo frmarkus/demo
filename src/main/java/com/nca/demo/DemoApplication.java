c9e53c39614a2fc3da005f9568e989d9c5e2b884
Markus Frauenfelder <Markus.Frauenfelder@netcetera.com>
2018-09-12 12:21:19 +0200

    ein bisschen weniger peinlich

--- src/main/java/com/nca/demo/DemoApplication.java
+++ src/main/java/com/nca/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("peinlicher test");
+		System.out.println("weniger peinlicher test");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
